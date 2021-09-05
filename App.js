2e4d102d6c0656b1876017784089c14c62717eaf
imrohit007 <rohitthakur9830@gmail.com>
2021-09-05 20:25:32 +0530

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,149 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import React, { Component } from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
+import Carousel from 'react-native-snap-carousel';
+
+
+export default class App extends Component {
+  state = {
+    showCarousel: false,
+    layoutType: 'tinder',
+    imageSearchTerms: [
+      'Books',
+      'Code',
+      'Girls',
+      'Dogs',
+    ]
+  }
+
+
+  toggleCarousel = () => {
+    this.setState({
+      showCarousel: !this.state.showCarousel
+    });
+  }
+
+  renderControls = () => {
+    return(
+      <View style={styles.container}>
+        
+        <TouchableOpacity
+          onPress={this.toggleCarousel}
+          style={styles.openButton}
+        >
+          <Text style={styles.openButtonText}>Open Carousel</Text>
+        </TouchableOpacity>
+      </View>
+    )
+  }
+
+  renderCarousel = () => {
+    return(
+      <View style={styles.carouselContainer}>
+        <View style={styles.closeButtonContainer}>
+          <TouchableOpacity
+            onPress={this.toggleCarousel}
+            style={styles.button}
+          >
+            <Text style={styles.label}>x</Text>
+          </TouchableOpacity>
+        </View>
+        <Carousel
+          layout={this.state.layoutType}
+          data={this.state.imageSearchTerms}
+          renderItem={this.renderItem}
+          sliderWidth={350}
+          itemWidth={350}
+        >
+        </Carousel>
+      </View>
+    );
+  }
+
+  renderItem = ({item}) => {
+    return (
+      <View style={styles.slide}>
+        <Image
+          style={styles.image}
+          source={{ uri: `https://source.unsplash.com/350x350/?${item}`}}
+        />
+        <Text style={styles.label}>{item}</Text>
+      </View>
+    );
+  }
+
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        {this.state.showCarousel ?
+          this.renderCarousel() :
+          this.renderControls()
+        }
+      </SafeAreaView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
   },
+  carouselContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#474747',
+    marginBottom:150,
+    marginTop:150,
+  },
+  closeButtonContainer: {
+    width: 350,
+    flexDirection: 'row',
+    justifyContent: 'flex-end'
+  },
+  slide: {
+    flex: 1,
+    marginTop:100,
+    marginBottom:100,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    width:350,
+    height: 350,
+  },
+  label: {
+    fontSize: 30,
+    padding: 40,
+    color: '#fff',
+    backgroundColor: '#474747'
+  },
+  openButton: {
+    padding: 10,
+    backgroundColor: '#000'
+  },
+  openButtonText: {
+    fontSize: 20,
+    padding: 20,
+    color: '#fff',
+  },
+  closeButton: {
+    padding: 10
+  },
+  picker: {
+    height: 150,
+    width: 100,
+    backgroundColor: '#fff'
+  }
 });
